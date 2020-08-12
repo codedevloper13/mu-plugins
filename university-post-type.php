e1db6ab43371049aa6971c4213e1488773857bbd
codedevloper13 <54003879+codedevloper13@users.noreply.github.com>
2020-08-12 12:22:52 +0530

    just upload
    asdas

---
+++ university-post-type.php
@@ -0,0 +1,35 @@
+<?php
+
+// Custom Event Post tYpe...
+
+function university_event_post_type() {
+    add_theme_support( 'post-thumbnails' );
+    register_post_type( 'event', array(
+        'show_in_rest'  =>true,
+        'supports'    => array(
+            'title',
+            'editor',
+            'thumbnail',
+            'excerpt',
+            'comments',
+            'revisions',
+
+        ),
+        'taxonomies'          => array( 'category', 'tag' ),
+        'rewrite'     => array( 'slug' => 'events' ),
+        'has_archive' => true,
+        'public'      => true,
+        'labels'    => array(
+            'name'          => 'Events',
+            'singular_name' => __( 'event' ),
+            'add_new_item'  => 'Add New Event',
+            'edit_item'     => 'Edit Event',
+            'all_items'     => 'All Event',
+            'update_item'   => 'Update Events',
+        ),
+        'menu_icon' => 'dashicons-calendar-alt',
+    ) );
+
+}
+
+add_action( 'init', 'university_event_post_type' );
